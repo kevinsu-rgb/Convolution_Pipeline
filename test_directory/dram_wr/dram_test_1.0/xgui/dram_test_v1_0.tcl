1de29300193ad1dc30701aae77301b790c1c886e
Kevin Su <kevin.su@ufl.edu>
2025-12-25 21:33:53 -0500

    created support for spartan 7 and began transition to systemverilog

--- test_directory/dram_wr/dram_test_1.0/xgui/dram_test_v1_0.tcl
+++ test_directory/dram_wr/dram_test_1.0/xgui/dram_test_v1_0.tcl
@@ -1,13 +1,6 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
-  #Adding Page
-  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  ipgui::add_param $IPINST -name "C_S00_AXI_DATA_WIDTH" -parent ${Page_0} -widget comboBox
-  ipgui::add_param $IPINST -name "C_S00_AXI_ADDR_WIDTH" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S00_AXI_BASEADDR" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "C_S00_AXI_HIGHADDR" -parent ${Page_0}
-
 
 }
 
